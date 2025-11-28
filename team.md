e9c89171023a5697b59569cafb264bac69a24831
Polina Zimina <polina.teamlead@university.com>
2025-11-28 15:06:56 +0300

    docs: add team info and unit tests

---
+++ team.md
@@ -0,0 +1,6 @@
+﻿# Команда разработки калькулятора
+
+Участники:
+- Polina Zimina (Team Lead)
+- Alice Smith (Developer) 
+- Bob Johnson (Developer)
---
+++ test_calculator.py
@@ -0,0 +1,8 @@
+﻿# test_calculator.py
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def test_addition(self):
+        calc = Calculator()
+        self.assertEqual(calc.add(2, 3), 5)
